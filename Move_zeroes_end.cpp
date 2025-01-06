6bf6bf0ce2e618f9f5d998be8061af3e0b0707d1
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-06 17:45:03 +0530

    Update Move_zeroes_end.cpp

--- Move_zeroes_end.cpp
+++ Move_zeroes_end.cpp
@@ -1,1 +1,26 @@
 //move all zeroes to end of array
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void moveZeroesToEnd(vector<int>& nums) {
+    int nonZeroIndex = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[nonZeroIndex++] = nums[i];
+        }
+    }
+    for (int i = nonZeroIndex; i < nums.size(); i++) {
+        nums[i] = 0;
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 0, 5, 3, 9, 0, 0, 5, 0, 6, 7};
+    moveZeroesToEnd(nums);
+    for (int i : nums) {
+        cout << i << " ";
+    }
+    return 0;
+}
